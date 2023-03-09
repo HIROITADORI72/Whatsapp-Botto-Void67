9e8645becf77750e51e3ac36300a999ab409d5f4
Senpai_Yuji <115178136+HIROITADORI72@users.noreply.github.com>
2023-03-09 18:41:10 +0200

    Update Sticker.ts

--- src/commands/Utils/Sticker.ts
+++ src/commands/Utils/Sticker.ts
@@ -27,8 +27,8 @@ export default class Command extends BaseCommand {
         parsedArgs.flags.forEach((flag) => (parsedArgs.joined = parsedArgs.joined.replace(flag, '')))
         const pack = parsedArgs.joined.split('|')
         const sticker = new Sticker(buffer, {
-            pack: pack[1] || '👾 𝐇𝐚𝐧𝐝𝐜𝐫𝐚𝐟𝐭𝐞𝐝 𝐅𝐨𝐫 𝐘𝐨𝐮 ',
-            author: pack[2] || '𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭𝐭𝐨 𝐕𝐨𝐢𝐝 👾',
+            pack: pack[1] || '👾 Dreaded',
+            author: pack[2] || 'Yuji',
             crop: parsedArgs.flags.includes('--stretch')
         })
         await sticker.build()
